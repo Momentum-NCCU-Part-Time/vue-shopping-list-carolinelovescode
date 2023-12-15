1051b90c9016d5f538a5141912c449a774e1e2f2
carolinelovescode <csuec23@gmail.com>
2023-12-14 21:17:04 -0500

    WIP GettingNotesToShow

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { ref } from 'vue'
+
+const lists = ref['']
+const listTitle = ref('')
 </script>
+
+// lists and their items on the page by Tuesday
 
 <template>
   <div>
     <h1>Vue Shopping List</h1>
     <!-- your code here -->
   </div>
+  <h2>{{ lists || 'You Got It All' }}</h2>
+
+  <input v-model="lists" placeholder="What Do You Need?" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/assets/components/ShopList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import App from '../../App.vue'
+
+const lists = ref([''])
+
+fetch('http://localhost:3000/notes/', {
+  method: 'GET'
+})
+  .then((res) => res.json())
+  .then((item) => (lists.value = item))
+</script>
+
+<template>
+  <App />
+  <div class="shop-list">
+    <h2>CarolineLovesShoppingList</h2>
+    <li></li>
+  </div>
+</template>
